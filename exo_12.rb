0beefa822e28ed4515ee5c7a006e4ecb2a3a2a0a
shirley Valeton <shirley.valeton88@gmail.com>
2024-10-01 13:30:03 +0200

    exo 12

---
+++ exo_12.rb
@@ -0,0 +1,7 @@
+puts "Bonjour, en quelle année êtes-vous née?"
+print "> " 
+user_birth_year = gets.chomp.to_i 
+(2024 - user_birth_year + 1).times do |i| 
+    year = i
+    puts "#{user_birth_year + i} - year : tu as 36 ans: tu avais #{year} ans" # (1)code pour calculer de l'année de naissance à aujourd'hui + texte tu avais ... ans
+end
