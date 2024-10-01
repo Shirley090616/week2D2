5eaf74890a6a44c1ee6d3be7311e80dc6452abff
shirley Valeton <shirley.valeton88@gmail.com>
2024-10-01 11:00:56 +0200

    ajout des commentaires

--- exo_04.rb
+++ exo_04.rb
@@ -1,4 +1,4 @@
 puts "Bonjour, quelle votre année de naissance ?"
-user_birth_year = gets.chomp.to_i
-user_year_100 = user_birth_year + 100
-puts "vous aurez 100 ans en l'année #{user_year_100} . " 
+user_birth_year = gets.chomp.to_i #l'utilisateur mets son année de naissance
+user_year_100 = user_birth_year + 100 #code pour calculer l'année des 100 ans
+puts "vous aurez 100 ans en l'année #{user_year_100} . " # question + reponse question
