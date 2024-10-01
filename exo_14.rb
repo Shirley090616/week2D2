ab4ae4c1a9b1a4a6d7748d82e01af6045e1c9cf5
shirley Valeton <shirley.valeton88@gmail.com>
2024-10-02 01:22:04 +0200

    creation fichier

--- exo_14.rb
+++ exo_14.rb
@@ -1,10 +1,12 @@
-faux_emails = [] # Programme pour générer une liste de faux emails
+fake_emails  = [] # Programme pour générer une liste de faux emails
 
 50.times do |i| # Formater le numéro pour avoir un zéro devant si nécessaire
-  numero = format('%02d', i + 1)
-  faux_email = "jean.dupont.#{numero}@email.fr"
-  faux_emails << faux_email
+  number = format('%02d', i + 1)
+  fake_email = "jean.dupont.#{number}@email.fr"
+  fake_emails << fake_email
 end
 
-emails_pairs = faux_emails.select { |email| email.match?(/\d{2}/) && email[-5].to_i.even? }
+even_emails = fake_emails.select {|email| email[-5].to_i.even?}
 # Filtrer et afficher uniquement les emails avec un nombre pair
+puts "Emails with an even number:"
+puts even_emails
