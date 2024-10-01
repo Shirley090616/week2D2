dd8e2355844fb0836fa7e16b4c4661a5c660665a
shirley Valeton <shirley.valeton88@gmail.com>
2024-10-01 23:27:38 +0200

    pas reussi

--- exo_12.rb
+++ exo_12.rb
@@ -1,13 +1,16 @@
 puts "Bonjour, en quelle année êtes-vous née?"
 print "> " 
 user_birth_year = gets.chomp.to_i 
-this_year = 2024
-user_age = this_year - user_birth_year
+current_year = 2024
+user_age = current_year - user_birth_year
 
 (user_age + 1).times do |i| 
-    current_year = user_birth_year + i
+  year = 1
+    
+    puts "#{user_birth_year + i} - année : tu avais #{year} ans"
   
     puts "#{current_year} - age: #{i} years"
+    current_year = user_birth_year + i
     
     puts "You were half your age." if i == user_age / 2
   end
