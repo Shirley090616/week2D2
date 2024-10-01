48cd1009d2e9a0d57c69cac26c2d00cc6fb64333
shirley Valeton <shirley.valeton88@gmail.com>
2024-10-01 13:19:23 +0200

    exo09+10

--- exo_10.rb
+++ exo_10.rb
@@ -1,6 +1,7 @@
 puts "Bonjour, en quelle année êtes-vous née?"
-print "> " # penser a mettre print pour reponse utilisateur
-user_birth_year = gets.chomp.to_i # l'utilisateur rentre son année
-(2024 - user_birth_year + 1).times do |i| # code pour calculer de l'année de naissance à aujourd'hui
-    puts user_birth_year + i # sa produit la liste des années
+print "> " 
+user_birth_year = gets.chomp.to_i 
+(2024 - user_birth_year + 1).times do |i| # (1)code pour calculer de l'année de naissance à aujourd'hui
+    year = i
+    puts "#{user_birth_year + i} - year : #{year} ans" # (1)code pour calculer de l'année de naissance à aujourd'hui
 end
