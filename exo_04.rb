9dd7d1536b435cf9943d38dfedc739bcb1f37bb6
shirley Valeton <shirley.valeton88@gmail.com>
2024-10-01 10:52:12 +0200

    changement des lignes

---
+++ exo_04.rb
@@ -0,0 +1,4 @@
+puts "Bonjour, quelle votre année de naissance ?"
+user_birth_year = gets.chomp.to_i
+user_year_100 = user_birth_year + 100
+puts "vous aurez 100 ans en l'année #{user_year_100} . " 
