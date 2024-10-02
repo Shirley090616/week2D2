9aa4c705cc26feec73b461d6aa35cbdb4e445846
shirley Valeton <shirley.valeton88@gmail.com>
2024-10-02 10:11:01 +0200

    mis commentaire

--- exo_15.rb
+++ exo_15.rb
@@ -1,8 +1,7 @@
 puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
 print "> "
 number_of_levels = gets.chomp.to_i
-if number_of_levels < 1 || number_of_levels > 25
-  puts "Veuillez entrer un nombre entre 1 et 25."
+if number_of_levels < 1 || number_of_levels > 25 puts # formule pour entrer un nombre entre 1 et 25.
 else
   puts "Voici la pyramide :"
   number_of_levels.times do |i|
